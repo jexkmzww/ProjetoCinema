ff5d9f7c52ad2b1aedf51cd6bed0790d0d9e118a
jexkmzww <backdoors@backdoors.bat>
2018-06-20 21:30:58 -0300

    Criado pacote Controle para atuar com o banco de dados

---
+++ src/Controle/ConexaoDB.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controle;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author backdoors
+ */
+public class ConexaoDB {
+    
+    public Statement statement;
+    public ResultSet resultSet;
+    private String driver = "org.postgresql.Driver";
+    private String caminho = "jdbc:postgresql://localhost:5432/projetocinema";
+    private String usuario = "postgres";
+    private String senha = "dudu";
+    public Connection connection;
+    
+    public void conexao (){
+        try {
+            System.setProperty("jdbc.Drivers", driver);
+            connection = DriverManager.getConnection(caminho, usuario, senha);
+            JOptionPane.showMessageDialog(null, "Conectado com sucesso!");
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Erro ao conectar com o banco de dados:\n" + ex);
+        }
+    }
+    
+}
